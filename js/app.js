364bd32c1c75c78f94870dd718e14d96df976fd5
GrimmOutlook <grimmoutlook87@gmail.com>
2015-01-31 14:45:37 -0500

    Created a click-handler for the Add button

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 // Problem: User interaction does not work
 // Solution: Add interactivity so the user can manage daily tasks
 var taskInput = document.getElementById("new-task");
-var addButton = getElementsByTagName("button")[0]; //first button
+var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTasksHolder = document.getElementById("incomplete-tasks");
 var completedTasksHolder = document.getElementById("completed-tasks");
 
 // Plan: 1. Add tasks
 var addTask = function (){
-
+  console.log("Add Task");
   // When button is pressed,
   // Create a new list item with the text from #new-task
     //input (checkbox)
@@ -20,7 +20,7 @@ var addTask = function (){
 
 // 2. Edit an existing task
 var editTask = function (){
-
+  console.log("Edit Task");
   //When the edit button is pressed:
     //if the class of the parent is .editMode
       //switch from .editMode
@@ -34,21 +34,29 @@ var editTask = function (){
 
 // 3. Delete an existing task
 var deleteTask = function(){
-
+  console.log("Delete Task");
   //When delete button is pressed
     //remove the parent list item from the ul
 };
 
 // 4. Mark a task as complete
 var taskCompleted = function(){
-
+  console.log("Task Completed");
   //When the checkbox is checked
     //Append the task list item to the #completed-tasks
 };
 
 // 5. Mark a task as incomplete
 var taskIncomplete = function(){
-
+  console.log("Task Incomplete");
   //When the checkbox is unchecked
     //Append the task list item to the #incomplete-tasks
 };
+
+
+//Set the click handler to the addTask function
+//addButton????
+addButton.onclick = addTask;
+
+
+
