97fe04ce325eeb6f4662b023393d1a35ec91529d
GrimmOutlook <grimmoutlook87@gmail.com>
2015-01-31 10:01:16 -0500

    Planning notes in app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
 // Problem: User interaction does not work
 // Solution: Add interactivity so the user can manage daily tasks
+
+// Plan: 1. Add tasks
+  // When button is pressed,
+  // Create a new list item with the text from #new-task
+    //input (checkbox)
+    //label
+    //input (text)
+    //button.edit
+    //button.delete
+    //Each of the above elements will need to be modified and appended
+
+
+
+
+// 2. Edit an existing task
+  //When the edit button is pressed:
+    //if the class of the parent is .editMode
+      //switch from .editMode
+      //label text to become the input's value
+    //else
+      //switch to .editMode
+      //input value becomes the label's text
+
+    //Toggle .editMode on the parent
+
+
+
+
+// 3. Delete an existing task
+
+
+
+
+// 4. Mark a task as complete
+
+
+
+
+// 5. Mark a task as incomplete
+
