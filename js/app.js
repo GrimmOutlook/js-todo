d86818802193886b7ec07fec700b2c82b6645a41
GrimmOutlook <grimmoutlook87@gmail.com>
2015-01-31 14:01:36 -0500

    Added selected elements to variables to attach events to

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 // Problem: User interaction does not work
 // Solution: Add interactivity so the user can manage daily tasks
+var taskInput = document.getElementById("new-task");
+var addButton = getElementsByTagName("button")[0]; //first button
+var incompleteTasksHolder = document.getElementById("incomplete-tasks");
+var completedTasksHolder = document.getElementById("completed-tasks");
 
 // Plan: 1. Add tasks
+var addTask = function (){
+
   // When button is pressed,
   // Create a new list item with the text from #new-task
     //input (checkbox)
@@ -10,11 +16,11 @@
     //button.edit
     //button.delete
     //Each of the above elements will need to be modified and appended
-
-
-
+};
 
 // 2. Edit an existing task
+var editTask = function (){
+
   //When the edit button is pressed:
     //if the class of the parent is .editMode
       //switch from .editMode
@@ -24,19 +30,25 @@
       //input value becomes the label's text
 
     //Toggle .editMode on the parent
-
-
-
+};
 
 // 3. Delete an existing task
+var deleteTask = function(){
 
-
-
+  //When delete button is pressed
+    //remove the parent list item from the ul
+};
 
 // 4. Mark a task as complete
+var taskCompleted = function(){
 
-
-
+  //When the checkbox is checked
+    //Append the task list item to the #completed-tasks
+};
 
 // 5. Mark a task as incomplete
+var taskIncomplete = function(){
 
+  //When the checkbox is unchecked
+    //Append the task list item to the #incomplete-tasks
+};
